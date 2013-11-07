dd09a34d60c3a02ca29a107c78b8bbeff03b5472
Scott QA Youmans <scotty@soundstrue.com>
2013-11-07 09:27:29 -0700

    Remove middle paragraph

--- scott.php
+++ scott.php
@@ -5,10 +5,6 @@ Pellentesque faucibus viverra metus, vitae rutrum dui dapibus id. Suspendisse in
 </p>
 
 <p>
-Proin venenatis, neque id suscipit imperdiet, felis libero accumsan velit, at interdum nulla tellus a nisl. Vestibulum quis urna sagittis, mollis diam ut; sagittis urna. Mauris eu tempus ipsum. Sed posuere dui hendrerit, commodo felis vel, fermentum sapien! Proin aliquam dui sed dictum pulvinar. Cras in lacinia leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam volutpat elementum velit id iaculis. Aenean mattis quam sed varius dictum. Aliquam pharetra elit non euismod sagittis. Praesent est est, aliquet sed placerat vitae, sagittis at metus?
-</p>
-
-<p>
 Vivamus pellentesque dignissim accumsan. Nullam urna nunc, semper nec fringilla vitae, feugiat nec nibh. Duis eget lobortis velit, vel condimentum elit! Sed odio lacus, semper non velit ac, ultricies accumsan turpis. Maecenas commodo quam in pellentesque aliquam. Aenean ac neque dictum, condimentum tortor ac, hendrerit nunc. Curabitur sit amet eros id arcu vehicula pharetra. Vivamus sit amet elit eu mauris lacinia fermentum quis nec urna. Nam et odio sem. Vestibulum aliquet ac mauris nec ullamcorper.
 </p>
 <p><a href="index.php">Back</a></p>
